9be0025b38e69c58ffc300af253dfe3c312ab2d3
jesusmirena <jesusamh16@gmail.com>
2021-11-25 23:47:35 -0500

    first commit - class component
    changing everything to class components

---
+++ client/src/components/Detail.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { getVideogameDetail } from "../actions/index";
+import styles from "./Detail.module.scss";
+import { Spinner } from "./spinner";
+
+export default function Detail(props) {
+  const dispatch = useDispatch();
+  const { id } = props.match.params;
+  const videogameDetail = useSelector((state) => state.videogameDetail);
+
+  useEffect(() => {
+    dispatch(getVideogameDetail(id));
+  }, [id, dispatch]);
+
+  return (
+    <div>
+      <Link to="/home">
+        <button className={styles.buttonBack}>Back</button>
+      </Link>
+      {videogameDetail ? (
+        <div className={styles.detailsContainer}>
+          <img
+            className={`${styles.col} ${styles.videogameImage}`}
+            src={
+              videogameDetail.background_image ||
+              "https://m.media-amazon.com/images/I/611fcGzpVUL.jpg"
+            }
+            alt=""
+          />
+          <div className={`${styles.col} ${styles.videogameDetails}`}>
+            <p className={styles.firstItem}>
+              <strong>Title:</strong> {videogameDetail.name}
+            </p>
+            <p>
+              <strong>Genres:</strong>{" "}
+              {videogameDetail.genres?.map((g) => g.name).join(", ")}
+            </p>
+            <p>
+              <strong>Description:</strong>
+              {videogameDetail.description_raw || videogameDetail.description}
+            </p>
+            <p>
+              <strong>Platforms:</strong>
+              {videogameDetail.id?.length > 7
+                ? videogameDetail.platforms?.map((p) => p.name).join(", ")
+                : videogameDetail.platforms?.map((p) => p.platform.name)
+                    .join(", ")}
+            </p>
+            <p>
+              <strong>Release Date:</strong>
+              {videogameDetail.released || "None"}
+            </p>
+            <p>
+              <strong>Rating:</strong>★{videogameDetail.rating}
+            </p>
+          </div>
+        </div>
+      ) : (
+        <Spinner />
+      )}
+    </div>
+  );
+}
