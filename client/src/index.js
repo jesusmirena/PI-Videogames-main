0939b15923032709ecf3d1827c2b29fdef54759a
jesusmirena <jesusamh16@gmail.com>
2021-11-25 23:49:38 -0500

    Revert "first commit - class component"
    This reverts commit 9be0025b38e69c58ffc300af253dfe3c312ab2d3.

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {Provider} from 'react-redux'
-import {store} from './store'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
-);
-
-
+ReactDOM.render(<App />, document.getElementById("root"));
